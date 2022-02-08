e069b0664afe4cf83e5d7dba0c81ac887a77c19e
toonbrime <83805291+toonbrime@users.noreply.github.com>
2022-02-08 12:35:07 +0500

    Update values.js

--- access/values.js
+++ access/values.js
@@ -1,24 +1,24 @@
 // Активировать документы (true - включены, false - отключены)
 var isRightsEnabled = true; // Права
-var isCovidCertificateEnabled = true; // COVID-сертификат
+var isCovidCertificateEnabled = false; // COVID-сертификат
 
 // Общее
-var birthdate = "01.01.2001"; // Дата рождения
-var fullname = "Фамилия Имя Отчество" // ФИО
-var name = "Имя" // Имя
+var birthdate = "16.03.2003"; // Дата рождения
+var fullname = "Федай Василь Юрійович " // ФИО
+var name = "Василiй" // Имя
 
 // Паспорт
-var passport_id = "123456789" // Номер паспорта
+var passport_id = "0276196380" // Номер паспорта
 
 // КПП
-var kpp_id = "0123456789" // ИНН
+var kpp_id = "02766197631" // ИНН
 
 // Права
-var rights_valid_until = "01.01.2022" // Права "Дійсні до"
-var rights_categories = "A, B" // Права "Категорії"
-var rights_tsc = "ТСЦ 8631" // Права "Видав
-var rights_id = "KBE968639" // Права "Серія та номер""
+var rights_valid_until = "01.01.2029" // Права "Дійсні до"
+var rights_categories = "A" // Права "Категорії"
+var rights_tsc = "ТСЦ 8644" // Права "Видав Ділянкою Видачі Прав Олександрійського району"
+var rights_id = "KAC5039" // Права "Нет"
 
 // COVID-сертификат
 var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
-var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55646740383788<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
